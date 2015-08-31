8eb041d6e09ec63733610c20846d5fd25cb293be
kaushikmukunda <kmukunda@qualcomm.com>
2015-08-30 19:13:06 -0700

    Adjust dimensions for phone screen

--- app/src/main/java/material/km/com/onestopshop/card/CardHolder.java
+++ app/src/main/java/material/km/com/onestopshop/card/CardHolder.java
@@ -1,14 +1,11 @@
 package material.km.com.onestopshop.card;
 
 import android.content.Context;
-import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import material.km.com.onestopshop.R;
